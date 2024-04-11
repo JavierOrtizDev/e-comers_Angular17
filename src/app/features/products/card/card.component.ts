43fcc600360279aaebf06b82f5e1c4e0bd5e160a
JavierOrtiz <javierorferdev@gmail.com>
2024-04-11 10:06:59 +0200

    refactorRoutersProducts

--- src/app/features/products/card/card.component.ts
+++ src/app/features/products/card/card.component.ts
@@ -1,14 +1,19 @@
 import { CurrencyPipe, SlicePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Product } from '@shared/model/product.interface';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CurrencyPipe, SlicePipe],
+  imports: [CurrencyPipe, SlicePipe, RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
 export class CardComponent {
   product = input.required<Product>();
+  @Output() addToCartEvent = new EventEmitter<Product>();
+  onAddToCart(): void {
+    this.addToCartEvent.emit(this.product());
+  }
 }
