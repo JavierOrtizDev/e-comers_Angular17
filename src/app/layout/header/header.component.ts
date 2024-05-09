571c8ad97be71d36eed709e675649bfaa4196368
JavierOrtiz <javierorferdev@gmail.com>
2024-05-09 13:45:48 +0200

    acabadoElDetails

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  showCart = signal<boolean>(false);
 }
